cfd16f08174397cd41e94cded9b765fba81329d1
Simon Lemoine <simon.lemoine@thetribe.io>
2023-06-30 10:13:45 +0200

    Feat: export with filters

--- server/controllers/exportData.js
+++ server/controllers/exportData.js
@@ -3,9 +3,8 @@ module.exports = ({ strapi }) => ({
         ctx.body = strapi.plugin('export-data').service('exportData').getContentTypes();
     },
     async exportContentType(ctx) {
-        const { contentType } = ctx.params;
         try {
-            const file = await strapi.plugin('export-data').service('exportData').exportContentType(ctx, contentType);
+            const file = await strapi.plugin('export-data').service('exportData').exportContentType(ctx);
             ctx.body = file;
             ctx.set('Content-disposition', 'attachment; filename=export.csv');
             ctx.set('Content-type', 'text/csv');
--- server/routes/exportData.js
+++ server/routes/exportData.js
@@ -11,7 +11,7 @@ module.exports = {
             },
         }, {
             method: 'GET',
-            path: '/export/:contentType',
+            path: '/export',
             handler: 'exportData.exportContentType',
             config: {
                 policies: [],
